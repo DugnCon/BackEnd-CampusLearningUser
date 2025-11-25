b8f64be60df926dd3f05cd36e2cdcee2664aab18
DugnCon <damdaidung9b@gmail.com>
2025-11-26 02:22:57 +0700

    up

--- src/main/java/com/javaweb/config/WebSecurityConfig.java
+++ src/main/java/com/javaweb/config/WebSecurityConfig.java
@@ -159,7 +159,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .baseUri("/oauth2/authorization") // Spring default
                 .and()
                 .redirectionEndpoint()
-                .baseUri("/oauth2/callback/*")
+                .baseUri("/api/oauth2/callback/*")
                 .and()
                 .successHandler((request, response, authentication) -> { //Nó sẽ callback về BE để làm token rồi gửi lại lên Fe để check Authenticated
 
